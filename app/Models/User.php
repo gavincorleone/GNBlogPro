b5ddc1f06e9d04555e35704160ea932d44c6a76e
gavincorleone <gavin_corleone@163.com>
2019-04-02 09:52:05 +0800

    用户微博列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,7 +49,7 @@ class User extends Authenticatable
             $user->activation_token = str_random(30);
         });
     }
-    public function stauses()
+    public function statuses()
     {
         return $this->hasMany(Status::class);
     }
