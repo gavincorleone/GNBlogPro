56be65c79b9bb80f45425a069174b7d0c50a8fb1
gavincorleone <gavin_corleone@163.com>
2019-04-02 11:05:30 +0800

    删除微博

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Policies\UserPolicy;
 use Illuminate\Support\Facades\Gate;
 use \App\Models\User;
+use \App\Models\Status;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -17,6 +18,8 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
          'App\Models' => 'App\Policies\ModelPolicy',
         User::class  => \App\Policies\UserPolicy::class,
+        Status::class => \App\Policies\StatusPolicy::class,
+
 
     ];
 
